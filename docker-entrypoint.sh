9baeff841f046dfe517cb8d70de1a43568ad9b85
HermesG96 <hermes@dgcloudops.com>
2026-07-16 17:10:19 +0000

    fix: run nginx as root (not via su), fix log/cache permissions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,15 +2,14 @@
 set -e
 
 # BACKEND_URL default: https://api-estadistico.dgcloudops.com
-# (el backend ya expuesto en esa URL)
 BACKEND_URL="${BACKEND_URL:-https://api-estadistico.dgcloudops.com}"
 export BACKEND_URL
 
 # Generar nginx.conf desde template con envsubst
 envsubst '${BACKEND_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
-# Asegurar permisos para nginx user
-chown -R nginx:nginx /var/cache/nginx /var/run /etc/nginx/conf.d
+# Arreglar permisos para que nginx user pueda escribir logs/cache
+# (nginx master corre como root, workers como nginx)
+chown -R nginx:nginx /var/log/nginx /var/cache/nginx /var/run /etc/nginx/conf.d
 
-# Iniciar nginx como non-root
-exec dumb-init -- su -s /bin/sh nginx -c "nginx -g 'daemon off;'"
+exec dumb-init -- nginx -g "daemon off;"
